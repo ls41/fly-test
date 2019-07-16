265f53c28036d01ef86db42cb1cd42394afc8814
lsy <ls41@qq.com>
2019-07-16 11:30:19 +0800

    修改：添加随机判断

--- wechat/src/main/java/com/lsy/wechat/utils/TestRandomUtil.java
+++ wechat/src/main/java/com/lsy/wechat/utils/TestRandomUtil.java
@@ -2,10 +2,18 @@ package com.lsy.wechat.utils;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TestRandomUtil {
 	public static Set<Integer> get(int cap, int size) {
 		HashSet set = new HashSet();
+		if (cap > size)
+			throw new RuntimeException();
+		if (cap == size) {
+			set = (HashSet) Stream.iterate(0, item -> item + 1).limit(size).collect(Collectors.toSet());
+		}
+
 		do {
 			set.add((int) (Math.random() * size));
 			//如果容量等于100  跳出循环
