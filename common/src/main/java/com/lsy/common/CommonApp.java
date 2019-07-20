b7b839a7918f897afb4997baa56d3d7516f6e611
lsy <ls41@qq.com>
2019-07-20 13:56:02 +0800

    修改：打包插件修改

--- common/src/main/java/com/lsy/common/CommonApp.java
+++ common/src/main/java/com/lsy/common/CommonApp.java
@@ -1,11 +1,11 @@
-package com.lsy.common;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class CommonApp {
-	public static void main(String[] args) {
-		SpringApplication.run(CommonApp.class);
-	}
-}
+//package com.lsy.common;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class CommonApp {
+//	public static void main(String[] args) {
+//		SpringApplication.run(CommonApp.class);
+//	}
+//}
